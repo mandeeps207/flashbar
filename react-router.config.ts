1b6450c0aa00f8e57d8751f9d80eeac9d0d9b01b
mandeep singh <mandeeps207@outlook.com>
2026-06-01 18:06:23 +0530

    vercel issue resolved 404

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  ssr: true,
   presets: [vercelPreset()],
 } satisfies Config;
